806ad03eb189f187f2d1e337672fda15e3951bfe
Jeffrey Marvin <jefftm@verizon.net>
2010-03-23 04:48:21 -0400

    Commenting Seems to be working.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
 
-  def new
-    @comment = Comment.new({:comment_text => params[":comment_text"], :document_id => params[':document_id'], :node_id => params[':node_id']})
+  def create
+    @comment = Comment.new({:comment_text => params[:comment_text], :document_id => params[:document_id], :node_id => params[:node_id]})
     @comment.save
   end
 
   def show
-    @comment = Comment.all(:conditions => ["document_id = ? AND node_id = ?", params[:document_id], params[:document_id]], :order => "created_at DESC") 
+    @comment = Comment.all(:conditions => "document_id = #{params[:id]} AND node_id = '#{params[:node_id]}'", :order => "created_at DESC") 
   end
 
 end
